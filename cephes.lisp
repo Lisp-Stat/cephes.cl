7f7b289d6c566e1edcd851e73326acb41e09c719
Symbolics <steve@nunez.org>
2020-11-18 11:42:13 +0800

    Fix inverse erf implementation
    Cephes ntdri is not exactly the inverse error function we expect. The
    newly exported erfinv matches the values returned by Python.

--- cephes.lisp
+++ cephes.lisp
@@ -10,7 +10,7 @@
 (cffi:defcfun "igamc" :double (a :double)(y :double)) ;Complemented incomplete Gamma integral
 
 ;; gamma.c
-(cffi:defcfun "lgam"  :double (a :double)(y :double))  ;Returns the base e logarithm of the absolute value of the Gamma function of the argument.
+(cffi:defcfun "lgam"  :double (x :double))  ;Returns the base e logarithm of the absolute value of the Gamma function of the argument.
 (cffi:defcfun "gamma" :double (x :double)) ; Returns Gamma function of the argument.  The result is correctly signed.
 
 
@@ -34,18 +34,20 @@
 (cffi:defcfun "erf"  :double (x :double))
 (cffi:defcfun "erfc" :double (x :double))
 
-;; ntdri.c
-(cffi:defcfun "ndtri" :double (y :double)) ;Returns the argument, x, for which the area under the Gaussian probability density function
-					   ;(integrated from minus infinity to x) is equal to y.
+;; ndtri.c
+(cffi:defcfun "ndtri" :double (y :double)) ;Inverse of the error function
 
-;; erfinv.c
-(cffi:defcfun "erfinv"  :double (y :double)) ;Inverse of the error function
-(cffi:defcfun "erfinvc" :double (y :double)) ;Inverse of the complementary error function.
+(defun erfinv (y)			; This is the same calculation Python uses. Not accurate when y is much less than 1
+  "Inverse of the error function"	; See https://github.com/scipy/scipy/issues/12758
+  (* (cephes:ndtri (* 0.5d0
+		      (1+ y)))
+     (/ 1 (sqrt 2))))
+
 
 
 #| These are left to do
 airy.c          chbevl.c    dd_real_idefs.h  exp10.c   gammasgn.c            k0.c          nbdtr.c                sindg.c   unity.c
-bdtr.c          chdtr.c     ellie.c          exp2.c    gdtr.c      igami.c   k1.c                     psi.c       sinpi.c   
+bdtr.c          chdtr.c     ellie.c          exp2.c    gdtr.c      igami.c   k1.c                     psi.c       sinpi.c
 besselpoly.c    const.c     ellik.c          expn.c    hyp2f1.c              kn.c                     rgamma.c    spence.c  yn.c
                 dawsn.c     ellpe.c          expn.h    hyperg.c              kolmogorov.c  owens_t.c  round.c     stdtr.c   yv.c
 btdtr.c         dd_idefs.h  ellpj.c          fdtr.c    i0.c                  lanczos.c     pdtr.c                 struve.c  zeta.c
--- package.lisp
+++ package.lisp
@@ -17,7 +17,7 @@
 	   #:incbet			; Lower incomplete beta, normalised
 	   #:incbi			; Inverse of incomplete beta integral
 	   #:ntdr			; Area under the Gaussian probability density function
-	   #:ntdri			; Returns x such that the area under the Gaussian PDF is equal to y.
+	   #:ndtri			; Returns x such that the area under the Gaussian PDF is equal to y.
 	   #:erf			; Error function
 	   #:erfc			; Complementary error function
 	   #:erfinv			; Inverse of the error function
